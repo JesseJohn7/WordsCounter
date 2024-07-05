0c6e334839c627bf6d849c68cefe46b849c131ff
JesseJohn7 <143970775+JesseJohn7@users.noreply.github.com>
2024-07-05 12:36:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* var message = "Hello "
+var name = prompt(" What is your name?")
+alert(message + name); */
+
+/* 
+var postLength = prompt(" Compose your text? ");
+var postUnder250 = postLength.slice(0,250); */
+/* alert ("You have written " + postLength.length + " characters you have " + (250-postLength.length) + " character's remaining"); */
+alert(prompt("compose your text;").slice(0,250));
